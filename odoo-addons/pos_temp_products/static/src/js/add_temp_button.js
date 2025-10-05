48c6ff3fddda529972236fd5404e744d804b678e
Leandro <leandroviscolungo@gmail.com>
2025-10-05 16:02:07 -0300

    lo de gpt code no funciona

--- odoo-addons/pos_temp_products/static/src/js/add_temp_button.js
+++ odoo-addons/pos_temp_products/static/src/js/add_temp_button.js
@@ -1,69 +1,21 @@
 /** @odoo-module */
 
 import { ProductScreen } from '@point_of_sale/app/screens/product_screen/product_screen';
-import { _t } from '@web/core/l10n/translation';
-import { TempProductPopup } from './temp_popup';
+import { openTempProductDialog } from './temp_product_handler';
 import { patch } from '@web/core/utils/patch';
 
-console.log('[pos_temp_products] Loading add_temp_button.js');
+const originalSetup = ProductScreen.prototype.setup;
 
-patch(ProductScreen.prototype, {
+patch(ProductScreen.prototype, 'pos_temp_products_product_screen_patch', {
     setup() {
-        super.setup();
-        console.log('[pos_temp_products] ProductScreen setup() patched');
+        originalSetup.apply(this, arguments);
     },
 
     async onClickAddTempProduct() {
-        console.log('[pos_temp_products] Button clicked!');
-        
-        const { confirmed, payload } = await this.popup.add(TempProductPopup, {
-            title: _t('Agregar productos temporales'),
+        await openTempProductDialog({
+            pos: this.pos,
+            popup: this.popup || this.env.services?.popup,
+            notification: this.notification || this.env.services?.notification,
         });
-        
-        if (!confirmed || !payload || !payload.rows?.length) {
-            console.log('[pos_temp_products] Popup cancelled or no data');
-            return;
-        }
-        
-        console.log('[pos_temp_products] Popup data:', payload);
-        
-        const pos = this.pos;
-        const order = pos.get_order();
-        if (!order) {
-            console.log('[pos_temp_products] No active order');
-            return;
-        }
-
-        // Find temp product by name (simple approach)
-        const allProducts = pos.db.get_product_by_category(0) || [];
-        const tempProduct = allProducts.find(
-            (p) => p.display_name === 'Producto temporal POS' || p.name === 'Producto temporal POS'
-        );
-        
-        if (!tempProduct) {
-            console.log('[pos_temp_products] Temp product not found in POS');
-            alert('Producto temporal no encontrado. Asegúrate de que esté disponible en el POS.');
-            return;
-        }
-
-        console.log('[pos_temp_products] Found temp product:', tempProduct.name);
-
-        for (const row of payload.rows) {
-            if (!row.name || row.qty <= 0 || row.price < 0) continue;
-            
-            console.log('[pos_temp_products] Adding row:', row);
-            
-            const line = order.add_product(tempProduct, {
-                quantity: row.qty,
-                merge: false,
-            });
-            
-            if (line) {
-                line.set_unit_price(row.price);
-                line.temp_name = row.name;
-                line.is_temp_line = true;
-                console.log('[pos_temp_products] Line added successfully');
-            }
-        }
     },
 });
--- odoo-addons/pos_temp_products/static/src/js/orderline_patch.js
+++ odoo-addons/pos_temp_products/static/src/js/orderline_patch.js
@@ -1,11 +1,20 @@
 /** @odoo-module */
-import { Orderline } from '@point_of_sale/app/store/models';
+
+import { PosOrderline } from '@point_of_sale/app/models/pos_order_line';
 import { patch } from '@web/core/utils/patch';
 
-patch(Orderline.prototype, 'pos_temp_products_orderline_patch', {
+const proto = PosOrderline.prototype;
+const superExportJson = proto.export_as_JSON;
+const superExportForPrinting = proto.export_for_printing;
+const superSetFullProductName = proto.setFullProductName;
+const superGetFullProductName = proto.getFullProductName;
+const orderDisplayDescriptor = Object.getOwnPropertyDescriptor(proto, 'orderDisplayProductName');
+const orderDisplayGetter = orderDisplayDescriptor?.get;
+
+patch(proto, 'pos_temp_products_orderline_patch', {
     export_as_JSON() {
-        const json = Orderline.prototype.export_as_JSON.call(this);
-        json.is_temp_line = this.is_temp_line || false;
+        const json = superExportJson.apply(this, arguments);
+        json.is_temp_line = Boolean(this.is_temp_line);
         if (this.temp_name) {
             json.temp_name = this.temp_name;
         }
@@ -13,12 +22,42 @@ patch(Orderline.prototype, 'pos_temp_products_orderline_patch', {
     },
 
     export_for_printing() {
-        const json = Orderline.prototype.export_for_printing.call(this);
+        const json = superExportForPrinting.apply(this, arguments);
         if (this.is_temp_line && this.temp_name) {
             json.product_name = this.temp_name;
             json.full_product_name = this.temp_name;
             json.name = this.temp_name;
         }
         return json;
+    },
+
+    setFullProductName() {
+        superSetFullProductName.apply(this, arguments);
+        if (this.is_temp_line && this.temp_name) {
+            this.full_product_name = this.temp_name;
+        }
+    },
+
+    getFullProductName() {
+        if (this.is_temp_line && this.temp_name) {
+            return this.temp_name;
+        }
+        return superGetFullProductName.apply(this, arguments);
+    },
+
+    get orderDisplayProductName() {
+        if (this.is_temp_line && this.temp_name) {
+            return {
+                name: this.temp_name,
+                attributeString: '',
+            };
+        }
+        if (orderDisplayGetter) {
+            return orderDisplayGetter.call(this);
+        }
+        return {
+            name: this.product_id?.name,
+            attributeString: '',
+        };
     },
 });
